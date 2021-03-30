c600deeed9612e317a9b3e7d4ecda9e6de693604
SaahilArora <arorasaahil1994@gmail.com>
2021-03-30 19:46:30 +0530

    First Push

---
+++ src/main/java/com/example/demo/JenkinsController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+    @GetMapping("/getDetails")
+    public String getUserName(){return "Hi User, I'm Alive....";}
+}
