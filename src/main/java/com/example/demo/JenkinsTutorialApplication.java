4604f0be5c4d755dd58d883da5673adeb418401b
SaahilArora <arorasaahil1994@gmail.com>
2021-04-25 16:42:13 +0530

    Dependency Added

--- src/main/java/com/example/demo/JenkinsTutorialApplication.java
+++ src/main/java/com/example/demo/JenkinsTutorialApplication.java
@@ -2,11 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 //Demo App
 @SpringBootApplication
-public class JenkinsTutorialApplication {
+public class JenkinsTutorialApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JenkinsTutorialApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTutorialApplication.class, args);
